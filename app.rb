fd5badd5aac6a443c3cbdbc57f0c08acb9979243
Rkebede <rutht.kebede@gmail.com>
2020-05-11 21:45:11 -0500

    Done.

--- app.rb
+++ app.rb
@@ -13,15 +13,3 @@ class App < Sinatra::Base
 	end 
 
 end
-
-# In this view, use ERB tags to create a variable name. 
-# This variable should store the name Joe. 
-# Then, using ERB tags, say "Goodbye Joe" in an h1 tag. 
-# This should get rendered via a GET /goodbye route by your App controller in app.rb.
-
-# It should contain an h1 with the content Today
-
-# Using ERB tags, and the DateTime library, 
-# display today's date in a p tag. 
-# The date should be formatted to look something like this 
-# The date is Wednesday, November 18, 2015.
